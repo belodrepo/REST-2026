088f7bdc90f63b3641de24669ac289fb8e497874
belod <sxbravo@gmail.com>
2025-09-22 12:14:04 +0200

    Backend elkészült

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express"); //Express importálása
 const app = express(); //Express példányosítása
-const port = 3000; //Port beállítása
+//const port = 3000; //Port beállítása
 
 //Middleware - köztes alkalmazások
 app.use(express.json());
@@ -19,6 +19,7 @@ app.get('/hello', (req, res) => {
 
 app.get('/api/courses', (req, res) => {
     res.json(courses);
+    //console.log(courses);
 })
 
 
@@ -29,6 +30,8 @@ app.get('/api/courses/:id', (req, res) => {
     //A keresett elem nem található (404) státusz kód és hibaüzenet visszaadása
     if (!course) res.status(404).send('A megadott ID-val nem létezik kurzus!');
     res.json(course); //Visszadjuk a keresett kurzust
+    console.log(course);
+    console.log(req.params.id);
 })
 
 //POST végpont létrehozása kurzus adatok küldésére a szervernek
@@ -39,12 +42,33 @@ app.post('/api/courses', (req, res) => {
         name: req.body.name
     }
 courses.push(course); //Az új kurzus objektum hozzáadása a courses tömbhöz
-res.json(req.body); //A kibővített kurzus adatainak a lekérése JSON formátumban
+res.json({ message: 'Új elem hozzáadva!', data: req.body}); //A kibővített kurzus adatainak a lekérése JSON formátumban
 
 })
 
+//DELETE végpont a kurzus adatok törlésére
+app.delete('/api/courses/:id', (req, res) => {
+    //Keresés a tömbben ID (URL paraméter alapján)
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    //Ha nincs ilyen elem, akor 404-es státuszkódot és egy hibaüzenete adjon vissza
+    if (!course) res.status(404).send('A megadott azonosítóval nincs kurus!');
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+    res.json({ message: 'Sikeres adattörlés', data: req.body});
+})
 
+//PUT - a kiválasztott kurus adatainak a módosítása
+app.put('/api/courses/:id', (req, res) => {
+    //Keresés a tömbben ID (URL paraméter alapján)
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    //Ha nincs ilyen elem, akor 404-es státuszkódot és egy hibaüzenete adjon vissza
+    if (!course) res.status(404).send('A megadott azonosítóval nincs kurus!');
+    course.name = req.body.name;
+    res.json({ message: 'Sikeres adatmódosítás', data: req.body});
+})
 
+//Az adott munkamenethez (session) tartozó portszám meghatározása
+const port = process.env.PORT || 3000;
 //A webszerver elindítása
 app.listen(port, () => {
     console.log(`A webszerver figyel a localhost:${port} webcímen`);
